195714ab3de464edc2622db3dbb9e85f6cb2dc05
Michael Davis <mdavis332@gmail.com>
2018-01-25 09:12:40 -0600

    Update Compare-DomAdmin.ps1
    Changed file names to be more generic (whatever the group name is set to) so it can be used for monitoring any AD security group.
    Add logic to rename file-new.txt after initial detection back to file.txt to prevent multiple recurrent messages over the same user's addition to a group.

--- Compare-DomAdmin/Compare-DomAdmin.ps1
+++ Compare-DomAdmin/Compare-DomAdmin.ps1
@@ -3,29 +3,36 @@ $SmtpUser = 'email@host.local'
 $SmtpPassword = 'PasswordGoesHere5!'
 $Credentials = New-Object System.Management.Automation.PSCredential -ArgumentList $SmtpUser, $($SmtpPassword |
         ConvertTo-SecureString -AsPlainText -Force)
+$Group = 'Domain Admins'
 
 $MailParams = @{
     To         = 'support@host.local'
     From       = 'email@host.local'
-    Subject    = "Domain Admin Membership Changes | $result was added to the Group"
     Body       = "Scheduled Task run at $(Get-Date) from $($MyInvocation.MyCommand.Definition) on computer $($env:computername)"
     SmtpServer = 'smtp.office365.com'
     Credential = $Credentials
 }
 
-$diff = (Get-ADGroupMember -Identity "Domain Admins").Name
+$diff = (Get-ADGroupMember -Identity $Group).Name
 
-If (!(Test-Path "$PSScriptRoot\Admins.txt")) {
-    Add-Content -Path "$PSScriptRoot\Admins.txt" -Value $diff
+If (!(Test-Path "$PSScriptRoot\$Group.txt")) {
+    Add-Content -Path "$PSScriptRoot\$Group.txt" -Value $diff
 }
 
-$base = Get-Content "$PSScriptRoot\Admins.txt"
+# If file-new exists from previous run, remove old file.txt and rename file-new to file. 
+If (Test-Path "$PSScriptRoot\$Group-New.txt") {
+    Rename-Item -Path "$PSScriptRoot\$Group.txt" -NewName "$PSScriptRoot\$Group-Old-$((Get-Date).ToString("yyyyMMdd-hhmm")).txt" -Force
+	Rename-Item -Path "$PSScriptRoot\$Group-New.txt" -NewName "$PSScriptRoot\$Group.txt"
+}
+
+$base = Get-Content "$PSScriptRoot\$Group.txt"
+
 
 $result = (Compare-Object -ReferenceObject $base -DifferenceObject $diff |
         Where-Object {$_.SideIndicator -eq "=>"} |
         Select-Object -ExpandProperty InputObject) -join ", "
-
+		
 If ($result) {
-    Send-MailMessage @MailParams -UseSsl -Priority High
-    Add-Content -Path "$PSScriptRoot\Admins-New.txt" -Value $diff
+    Send-MailMessage @MailParams -UseSsl -Priority High -Subject "$Group Membership Changes | $result was added to the Group"
+    Add-Content -Path "$PSScriptRoot\$Group-New.txt" -Value $diff
 }
