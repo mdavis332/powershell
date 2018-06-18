e20441057e627c90c9832577f30d7ed6752b7205
Michael Davis <mdavis332@gmail.com>
2018-06-18 17:37:23 -0500

    Improved argument handling
    Some services can have spaces due to pathname and/or because of arguments after the executable (e.g.: c:\program files\neo4j\bin\prunsrv-amd64.exe //RS/neo4j). No spaces in the pathname will still be returned by the WQL filter in the detection script because there are spaces in the overall pathname but no quotes around the file path. This regex addition should better match and provide quotes only around the pathname but not the arguments.

--- SCCM_CI/UnquotedServicePathName/RemediationScript.ps1
+++ SCCM_CI/UnquotedServicePathName/RemediationScript.ps1
@@ -4,6 +4,13 @@ $NeedsQuotes = Get-CimInstance -ClassName Win32_Service -Filter $Filter
 
 # Wrap double quotes around the PathName
 foreach ($Item in $NeedsQuotes) {
+    # if there is a path with arguments, it may show up here due to the space before the args, so split after a file ext
+    # regex pattern below looks for everything up to a period followed 2-6 non-whitespace chars indicating a file ext
+    # that full path is treated as the service name and everything else as the arguments 
+    $MatchResult = $Item.PathName | Select-String -Pattern '(^.+\.\S{2,6})(.*)'
+    $ServicePath = "`"$($MatchResult.Matches.Groups[1].Value)`""
+    $ServiceArgs = $MatchResult.Matches.Groups[2].Value
+    $NewPath = "${ServicePath}${ServiceArgs}"
 
-    Invoke-CimMethod -InputObject $Item -MethodName change -Arguments @{PathName = "`"$($Item.PathName)`""}
+    Invoke-CimMethod -InputObject $Item -MethodName change -Arguments @{PathName = $NewPath }
 }
