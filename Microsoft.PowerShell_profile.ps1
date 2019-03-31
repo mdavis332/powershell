cc7526c1008e16ffafdb617fc53ff65393da36d5
Michael Davis <mdavis332@gmail.com>
2019-03-31 10:21:36 -0500

    Adding ShowToolTips back to PSReadLine
    Soo, for pwsh 5.1, -ShowToolTips does *not* appear to be enabled by default (only $true for default in pwsh 6). Adding it back.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -60,4 +60,4 @@ cd c:\
 
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
-Set-PSReadLineOption -HistoryNoDuplicates
+Set-PSReadLineOption -HistoryNoDuplicates -ShowToolTips
